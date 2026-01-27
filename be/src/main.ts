6c48673f8a681329121321720ef240345702cf3f
Roman Gorisek <roman.gorisek2@gmail.com>
2026-01-27 21:59:13 +0100

    add helmet

--- be/src/main.ts
+++ be/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
 
   app.enableCors({
     origin: 'http://localhost:5173', // Allow requests from your frontend's origin
