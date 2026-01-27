415179e886c3947d76318794abd5db03f73eb02e
Roman Gorisek <roman.gorisek2@gmail.com>
2026-01-27 10:49:01 +0100

    form design

--- fe/src/components/EventsCreate.vue
+++ fe/src/components/EventsCreate.vue
@@ -1,44 +1,58 @@
 <template>
-  <div class="q-pa-lg bg-grey-1">
-    <h4 class="text-h4 text-weight-bold q-ma-none q-mb-lg">New event</h4>
+  <div class="q-pa-lg bg-grey-1 row justify-center">
+    <div class="col-xs-12 col-md-8 col-lg-6">
+      <h4 class="text-h4 text-weight-bold q-ma-none q-mb-lg text-center">New event</h4>
 
-    <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-sm q-mt-lg">
-      <q-input
-        outlined
-        v-model="formData.name"
-        label="Name *"
-        :rules="[(val) => (val && val.length > 0) || 'Value is required']"
-      />
+      <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-sm q-mt-lg">
+        <div class="q-mb-md">
+          <q-item-label class="q-pb-xs">Name *</q-item-label>
+          <q-input
+            outlined
+            dense
+            v-model="formData.name"
+            :rules="[(val) => (val && val.length > 0) || 'Value is required']"
+          />
+        </div>
 
-      <q-input
-        outlined
-        type="textarea"
-        v-model="formData.description"
-        label="Description *"
-        :rules="[(val) => (val && val.length > 0) || 'Value is required']"
-      />
+        <div class="q-mb-md">
+          <q-item-label class="q-pb-xs">Description *</q-item-label>
+          <q-input
+            outlined
+            dense
+            type="textarea"
+            v-model="formData.description"
+            :rules="[(val) => (val && val.length > 0) || 'Value is required']"
+          />
+        </div>
 
-      <q-select
-        outlined
-        v-model="formData.type"
-        :options="typeOptions"
-        label="Type"
-        :rules="[(val) => val !== null || 'Value is required']"
-      />
+        <div class="q-mb-md">
+          <q-item-label class="q-pb-xs">Type *</q-item-label>
+          <q-select
+            outlined
+            dense
+            v-model="formData.type"
+            :options="typeOptions"
+            :rules="[(val) => val !== null || 'Value is required']"
+          />
+        </div>
 
-      <q-select
-        outlined
-        v-model="formData.priority"
-        :options="priorityOptions"
-        label="Priority"
-        :rules="[(val) => val !== null || 'Value is required']"
-      />
+        <div class="q-mb-md">
+          <q-item-label class="q-pb-xs">Priority *</q-item-label>
+          <q-select
+            outlined
+            dense
+            v-model="formData.priority"
+            :options="priorityOptions"
+            :rules="[(val) => val !== null || 'Value is required']"
+          />
+        </div>
 
-      <div>
-        <q-btn label="Submit" type="submit" color="primary" />
-        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
-      </div>
-    </q-form>
+        <div>
+          <q-btn label="Submit" type="submit" color="primary" />
+          <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+        </div>
+      </q-form>
+    </div>
   </div>
 </template>
 
@@ -65,7 +79,7 @@ const typeOptions = [
 const priorityOptions = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 const onSubmit = () => {
-  console.log('Form Submitted:', form.value)
+  console.log('Form Submitted:', formData.value)
   $q.notify({
     type: 'positive',
     message: 'Event created successfully',
